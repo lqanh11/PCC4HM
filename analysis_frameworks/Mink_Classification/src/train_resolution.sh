91f2d91c54d52cb3a02e21565516084e17ff9cf3
lqanh11 <lqanh@vnu.edu.vn>
2023-12-11 09:53:40 +0700

    create bash file for training the Classification models

---
+++ analysis_frameworks/Mink_Classification/src/train_resolution.sh
@@ -0,0 +1,31 @@
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_2048/voxelized_64"\
+                                             --num_points=2048 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_2048/voxelized_128"\
+                                             --num_points=2048 &&\
+                                        
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_2048/voxelized_256"\
+                                             --num_points=2048 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_2048/voxelized_512"\
+                                             --num_points=2048 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_2048/voxelized_1024"\
+                                             --num_points=2048 &&\   
+
+
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_1024/voxelized_64"\
+                                             --num_points=1024 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_1024/voxelized_128"\
+                                             --num_points=1024 &&\
+                                        
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_1024/voxelized_256"\
+                                             --num_points=1024 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_1024/voxelized_512"\
+                                             --num_points=1024 &&\
+
+python classification_modelnet10_voxelize.py --data_root="/media/avitech/Data/quocanhle/PointCloud/dataset/modelnet10/pc_modelnet10_ply_hdf5_1024/voxelized_1024"\
+                                             --num_points=1024 &&\                                                                                        

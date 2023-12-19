e1ec7d736490bef68d2874e79c75153bc304ad70
lqanh11 <lqanh@vnu.edu.vn>
2023-12-19 10:00:09 +0700

    add frameworks: PointNet/PointNet++

---
+++ analysis_frameworks/PointNet/PointNet/data_utils/ModelNetDataLoader_ply.py
@@ -0,0 +1,226 @@
+'''
+@author: Xu Yan
+@file: ModelNet.py
+@time: 2021/3/19 15:51
+'''
+import os
+import numpy as np
+import warnings
+import pickle
+
+from tqdm import tqdm
+import torch
+from torch.utils.data import Dataset
+
+warnings.filterwarnings('ignore')
+
+def read_ply_ascii_geo(filedir):
+    files = open(filedir)
+    data = []
+    for i, line in enumerate(files):
+        wordslist = line.split(' ')
+        try:
+            line_values = []
+            for i, v in enumerate(wordslist):
+                if v == '\n': continue
+                line_values.append(float(v))
+        except ValueError: continue
+        data.append(line_values)
+    data = np.array(data)
+    coords = data[:,0:3].astype('int')
+
+    return coords
+
+def write_ply_ascii_geo(filedir, coords):
+    if os.path.exists(filedir): os.system('rm '+filedir)
+    f = open(filedir,'a+')
+    f.writelines(['ply\n','format ascii 1.0\n'])
+    f.write('element vertex '+str(coords.shape[0])+'\n')
+    f.writelines(['property float x\n',
+                  'property float y\n',
+                  'property float z\n',
+            ])
+    f.write('end_header\n')
+    coords = coords
+    for p in coords:
+        f.writelines(f'{int(p[0])} {int(p[1])} {int(p[2])}\n')
+    f.close() 
+
+    return
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
+    pc = pc / m
+    return pc
+
+
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:,:3]
+    centroids = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        centroids[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    point = point[centroids.astype(np.int32)]
+    return point
+
+
+class ModelNetDataLoader_ply(Dataset):
+    def __init__(self, root, split_path, args, split='train', process_data=False):
+        self.root = root
+        self.npoints = args.num_point
+        self.process_data = process_data
+        self.uniform = args.use_uniform_sample
+        self.use_normals = args.use_normals
+        self.num_category = args.num_category
+        self.resolution = 2 ** 7 - 1
+        self.split_path = split_path
+
+        if self.num_category == 10:
+            self.catfile = os.path.join(self.split_path, 'modelnet10_shape_names.txt')
+        else:
+            self.catfile = os.path.join(self.split_path, 'modelnet40_shape_names.txt')
+
+        self.cat = [line.rstrip() for line in open(self.catfile)]
+        self.classes = dict(zip(self.cat, range(len(self.cat))))
+
+        shape_ids = {}
+        if self.num_category == 10:
+            shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.split_path, 'modelnet10_train.txt'))]
+            shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.split_path, 'modelnet10_test.txt'))]
+        else:
+            shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.split_path, 'modelnet40_train.txt'))]
+            shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.split_path, 'modelnet40_test.txt'))]
+
+        assert (split == 'train' or split == 'test')
+        shape_names = ['_'.join(x.split('_')[0:-1]) for x in shape_ids[split]]
+        self.datapath = [(shape_names[i], os.path.join(self.root, shape_names[i], split, shape_ids[split][i]) + '.ply') for i
+                         in range(len(shape_ids[split]))]
+        print('The size of %s data is %d' % (split, len(self.datapath)))
+
+        if self.uniform:
+            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts_fps.dat' % (self.num_category, split, self.npoints))
+            # self.save_dense = os.path.join(root, 'modelnet%d_%s_%d_dense.dat' % (self.num_category, split, self.resolution+1))
+        else:
+            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts.dat' % (self.num_category, split, self.npoints))
+            # self.save_dense = os.path.join(root, 'modelnet%d_%s_%d_dense.dat' % (self.num_category, split, self.resolution+1))
+
+        if self.process_data:
+            if not os.path.exists(self.save_path):
+                print('Processing data %s (only running in the first time)...' % self.save_path)
+                self.list_of_points = [None] * len(self.datapath)
+                self.list_of_labels = [None] * len(self.datapath)
+                
+                self.list_of_file_name = [None] * len(self.datapath)
+                self.list_of_dense_points = [None] * len(self.datapath)
+                self.list_of_labels = [None] * len(self.datapath)
+                
+
+                for index in tqdm(range(len(self.datapath)), total=len(self.datapath)):
+                    fn = self.datapath[index]
+                    cls = self.classes[self.datapath[index][0]]
+                    cls = np.array([cls]).astype(np.int32)
+                    point_set = read_ply_ascii_geo(fn[1]).astype(np.float32)
+                    
+                    self.list_of_file_name[index] = fn
+             
+                    ## convert coordinates to interger for PCGCv2
+                    xyz = point_set[:,0:3]
+                    # coordinates normalize to fixed resolution.
+                    for axis_index in range(np.shape(xyz)[1]):
+                        xyz[:,axis_index] = xyz[:,axis_index] - np.min(xyz[:,axis_index])
+                        xyz[:,axis_index] = xyz[:,axis_index] / np.max(xyz[:,axis_index])
+                        xyz[:,axis_index] = xyz[:,axis_index] * (self.resolution)
+                    # quantizate to integers. 
+                    xyz = np.round(xyz).astype('int')
+                    # points_normal[:,0:3] = xyz
+                    # keep unique
+                    xyz, indices = np.unique(xyz, axis=0, return_index=True)
+
+                    # points_normal = points_normal[indices, :] 
+                    # ## save_ply
+                    # shape_name = fn[1].split('/')[-2]
+                    # ply_filename = fn[1].split('/')[-1]
+                    # ply_shapename_dir = os.path.join(ply_folder_dir, shape_name)
+                    # os.makedirs(ply_shapename_dir, exist_ok=True)
+                    # ply_file_dir = os.path.join(ply_shapename_dir, ply_filename)
+                    # write_ply_ascii_geo(ply_file_dir, points_normal)
+                    # # print('save_ply: ', ply_file_dir)
+
+
+
+                    point_set = xyz                    
+                    self.list_of_dense_points[index] = xyz
+                    
+#                     print(fn, 'number of points after preprocessing for dense: ', xyz.shape)
+
+
+                    if self.uniform:
+                        point_set = farthest_point_sample(point_set, self.npoints)
+                    else:
+                        point_set = point_set[0:self.npoints, :]
+
+                    self.list_of_points[index] = point_set
+                    self.list_of_labels[index] = cls
+
+                with open(self.save_path, 'wb') as f:
+                    pickle.dump([self.list_of_points, self.list_of_labels], f)
+                # with open(self.save_dense, 'wb') as f:
+                #     pickle.dump([self.list_of_file_name, self.list_of_dense_points, self.list_of_labels], f)
+            else:
+                print('Load processed data from %s...' % self.save_path)
+                with open(self.save_path, 'rb') as f:
+                    self.list_of_points, self.list_of_labels = pickle.load(f)
+
+    def __len__(self):
+        return len(self.datapath)
+
+    def _get_item(self, index):
+        if self.process_data:
+            point_set, label = self.list_of_points[index], self.list_of_labels[index]
+        else:
+            fn = self.datapath[index]
+            cls = self.classes[self.datapath[index][0]]
+            label = np.array([cls]).astype(np.int32)
+            point_set = read_ply_ascii_geo(fn[1]).astype(np.float32)
+
+            if self.uniform:
+                point_set = farthest_point_sample(point_set, self.npoints)
+            else:
+                point_set = point_set[0:self.npoints, :]
+                
+        
+        point_set = pc_normalize(point_set)
+#         if not self.use_normals:
+#             point_set = point_set[:, 0:3]
+            
+      
+        return point_set, label[0]
+
+    def __getitem__(self, index):
+        return self._get_item(index)
+
+
+if __name__ == '__main__':
+    import torch
+
+    data = ModelNetDataLoader_ply('/data/modelnet40_ply_downsampled/', split='train')
+    DataLoader = torch.utils.data.DataLoader(data, batch_size=12, shuffle=True)
+    for point, label in DataLoader:
+        print(point.shape)
+        print(label.shape)
---
+++ analysis_frameworks/PointNet/PointNet/train_pointnet_cls.sh
@@ -0,0 +1,9 @@
+# 512, 1024, 2048, 4096, 8192, 16384, 32768
+
+# # python train_classification.py --model pointnet_cls --num_point 512 --log_dir pointnet_cls_512 &&\
+# python train_classification.py --model pointnet_cls --num_point 1024 --log_dir pointnet_cls_1024 &&\
+# python train_classification.py --model pointnet_cls --num_point 2048 --log_dir pointnet_cls_2048 &&\
+# python train_classification.py --model pointnet_cls --num_point 4096 --log_dir pointnet_cls_4096 &&\
+# python train_classification.py --model pointnet_cls --num_point 8192 --log_dir pointnet_cls_8192 &&\
+# python train_classification.py --model pointnet_cls --num_point 16384 --log_dir pointnet_cls_16384 &&\
+python train_classification.py --model pointnet_cls --num_point 32768 --log_dir pointnet_cls_32768 

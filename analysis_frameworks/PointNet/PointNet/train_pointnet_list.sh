fb7d498f30919a3887d777fe1fbe3bd1b8ca3971
lqanh11 <lqanh@vnu.edu.vn>
2023-12-21 10:23:57 +0700

    update: change to train PointNet++ with precise coordinates

--- analysis_frameworks/PointNet/PointNet/train_pointnet_list.sh
+++ analysis_frameworks/PointNet/PointNet/train_pointnet_list.sh
@@ -6,6 +6,7 @@
 # python train_classification.py --model pointnet_cls --num_point 4096 --log_dir pointnet_cls_4096 &&\
 # python train_classification.py --model pointnet_cls --num_point 8192 --log_dir pointnet_cls_8192 &&\
 # python train_classification.py --model pointnet_cls --num_point 16384 --log_dir pointnet_cls_16384 &&\
+
 python train_classification.py --model pointnet2_cls_ssg --num_point 1024 --resolution 64 --log_dir pointnet2_cls_ssg_r64_n1024 &&\
 python train_classification.py --model pointnet2_cls_ssg --num_point 1024 --resolution 128 --log_dir pointnet2_cls_ssg_r128_n1024 &&\
 python train_classification.py --model pointnet2_cls_ssg --num_point 1024 --resolution 256 --log_dir pointnet2_cls_ssg_r256_n1024 &&\

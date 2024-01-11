ad8f614ad2d88a722b8c406ed4ae04ec160c8d18
lqanh11 <lqanh@vnu.edu.vn>
2024-01-11 13:49:21 +0700

    change name for consistent

---
+++ scalable_frameworks/PCGC/train_scalable_MLP.sh
@@ -0,0 +1,73 @@
+LIST_CKPT_ORIGINAL=(
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_0.25_000/ckpts/epoch_10.pth"
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_0.5_000/ckpts/epoch_10.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_1.0_000/ckpts/epoch_10.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_2.0_000/ckpts/epoch_10.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_4.0_000/ckpts/epoch_10.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_6.0_000/ckpts/epoch_10.pth"
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGCv2/modelnet_dense_full_reconstruction_with_pretrained_alpha_10.0_000/ckpts/epoch_10.pth"        
+)
+
+LIST_CKPT_BASE=(
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/2024-01-11_00-04_encFIXa025_baseTRANc_mlp_resolution128_alpha16000.0_000/best_model/epoch_34.pth"
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/2024-01-11_10-38_encFIXa05_baseTRANc_mlp_resolution128_alpha16000.0_000/best_model/epoch_37.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/20231230_encFIXa1_baseTRANc4_keepcoords_resolution128_alpha320.0_000/best_model/epoch_194.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/20231230_encFIXa2_baseTRANc4_keepcoords_resolution128_alpha320.0_000/best_model/epoch_197.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/20231230_encFIXa4_baseTRANc4_keepcoords_resolution128_alpha320.0_000/best_model/epoch_234.pth"
+  # "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/20231230_encFIXa6_baseTRANc4_keepcoords_resolution128_alpha320.0_000/best_model/epoch_154.pth"
+  "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/2024-01-10_21-03_encFIXa10_baseTRANc_mlp_resolution128_alpha16000.0_000/best_model/epoch_36.pth"
+)
+RESOLUTION=128
+
+# LIST_CKPT_BASE=(
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-02_encFIXa025_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_9.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-06_encFIXa05_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_7.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-09_encFIXa1_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_7.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-12_encFIXa2_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_9.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-15_encFIXa4_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_10.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-19_encFIXa6_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_3.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-21_encFIXa10_baseTRANc4_keepcoords_resolution64_alpha320.0_000/best_model/epoch_22.pth"
+# )
+# RESOLUTION=64
+
+# LIST_CKPT_BASE=(
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_14-48_encFIXa025_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_16.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_15-20_encFIXa05_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_10.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_15-39_encFIXa1_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_9.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_15-57_encFIXa2_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_9.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_16-15_encFIXa4_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_15.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_16-41_encFIXa6_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_8.pth"
+#   "/media/avitech/Data/quocanhle/PointCloud/logs/PCGC_scalable/logs_ModelNet10/cls_only/KeepCoordinates/2024-01-05_17-04_encFIXa10_baseTRANc4_keepcoords_resolution256_alpha320.0_000/best_model/epoch_7.pth"
+# )
+# RESOLUTION=256
+
+LIST_ALPHA=(
+    "encFIXa025_baseTRANcMLP_scalable"
+    "encFIXa05_baseTRANcMLP_scalable"
+    # "encFIXa1_baseFIXc4_enhaTRAINc8b3RB_Quantize_MSE_KeepCoords"
+    # "encFIXa2_baseFIXc4_enhaTRAINc8b3RB_Quantize_MSE_KeepCoords"
+    # "encFIXa4_baseFIXc4_enhaTRAINc8b3RB_Quantize_MSE_KeepCoords"
+    # "encFIXa6_baseFIXc4_enhaTRAINc8b3RB_Quantize_MSE_KeepCoords"
+    "encFIXa10_baseTRANcMLP_scalable"
+)
+
+# Get the length of one of the arrays (assuming both arrays have the same length)
+length_ckpt=${#LIST_CKPT_ORIGINAL[@]}
+
+for ((i=0; i<$length_ckpt; i++)); do
+    ckptdir_original=${LIST_CKPT_ORIGINAL[i]}
+    ckptdir_base=${LIST_CKPT_BASE[i]}
+
+    prefix_text=${LIST_ALPHA[i]}
+
+    echo "$prefix_text , $ckptdir_original"
+    python train_scalable_MLP.py --alpha 0.5\
+                             --resolution $RESOLUTION\
+                             --prefix $prefix_text\
+                             --init_ckpt_original $ckptdir_original\
+                             --init_ckpt_base $ckptdir_base
+    # for i in {16000,320,160,40,1}
+    # do
+    #     python train_cls_only.py --alpha $i --resolution 128 --prefix $prefix_text --init_ckpt_original $ckptdir
+    # done
+done
